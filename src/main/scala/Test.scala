f71154d6bf5ca76bfac01f7b9034277d6c996fee
Federico Bravetti <federicobravetti802@gmail.com>
2024-12-15 12:18:23 +0100

    chore: add scoverage3

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -4,3 +4,16 @@ object asd {
     println("Hello, world!")
   }
 }
+class Person() {
+  val name = "John"
+  val age  = 30
+
+  def greet() = {
+    println(s"Hello, my name is $name and I am $age years old.")
+  }
+
+  def greet2() = {
+    println(s"Hello, my name is $name and I am $age years old.")
+  }
+
+}
